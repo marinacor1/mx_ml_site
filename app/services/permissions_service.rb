01b4a217ece4addeef08f08364409b733d27d59c
Marina Corona <marinaspam1234@gmail.com>
2017-09-29 17:28:09 -0600

    Welcome path redirection works

--- app/services/permissions_service.rb
+++ app/services/permissions_service.rb
@@ -14,6 +14,7 @@ class PermissionsService
   end
 
   def allow?
+    binding.pry
     case
     when @a_guest.role == 'Admin' then admin_permissions
     when @a_guest.role == 'baf' then bridesmaid_and_family_permissions
--- spec/features/unregistered_guest_can_login_spec.rb
+++ spec/features/unregistered_guest_can_login_spec.rb
@@ -6,14 +6,32 @@ RSpec.feature 'unregistered guest can login' do
 
     visit root_path
 
+
     expect(current_path).to eq login_path
 
-    within('.login-user-form') do
-      fill_in 'code', with: "abc123"
-    end
+    fill_in 'Password:', with: "abc123"
 
     click_on 'Submit'
 
     expect(current_path).to eq welcome_path
+
+    expect(page).to have_content 'travel'
+  end
+
+  scenario 'they add incorrect code and view error message' do
+    guest = Guest.create(first_name1: 'Jodi', last_name2: 'Salcedo', code: 'abc123', role: 'guest')
+
+    visit root_path
+
+    expect(current_path).to eq login_path
+
+    fill_in 'Password:', with: "232"
+
+
+    click_on 'Submit'
+
+    expect(current_path).to eq login_path
+
+    expect(page).to have_content 'Lawrence'
   end
 end
